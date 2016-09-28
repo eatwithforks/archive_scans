1383f9d852da81ce2d6ec34b84816a4ca4afe24c
Jye Lee <jlee@cloudpassage.com>
2016-09-28 15:47:05 -0700

    add query_fetcher

---
+++ lib/query_fetcher.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+require_relative 'settings_controller'
+
+# Returns argument for scans endpoint
+class QueryFetcher
+  def initialize
+    @raw_params = Settings.params
+  end
+
+  def fetch
+    customize(filter_whitelist_params)
+  end
+
+  def whitelist
+    [:since, :until, :module]
+  end
+
+  def filter_whitelist_params
+    @raw_params.select { |key, _| whitelist.include? key }
+  end
+
+  def customize(filtered)
+    result = ''
+    filtered.each { |key, value| result += "#{key}=#{value}&" }
+  end
+end

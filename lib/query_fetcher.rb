0425e902b00a6b6dc50b62abcfa8b185f3932778
Jye Lee <jlee@cloudpassage.com>
2016-09-29 13:20:23 -0700

    fix: should return customized string instead of hash

--- lib/query_fetcher.rb
+++ lib/query_fetcher.rb
@@ -3,25 +3,24 @@ require_relative 'settings_controller'
 
 # Returns argument for scans endpoint
 class QueryFetcher
-  def initialize
-    @raw_params = Settings.params
-  end
+  class << self
+    def fetch
+      customize(filter_whitelist_params)
+    end
 
-  def fetch
-    customize(filter_whitelist_params)
-  end
+    def whitelist
+      [:since, :until, :module]
+    end
 
-  def whitelist
-    [:since, :until, :module]
-  end
+    def filter_whitelist_params
+      Settings.params.select { |key, _| whitelist.include? key }
+    end
 
-  def filter_whitelist_params
-    @raw_params.select { |key, _| whitelist.include? key }
-  end
-
-  def customize(filtered)
-    return if filtered.empty?
-    result = ''
-    filtered.each { |key, value| result += "#{key}=#{value}&" }
+    def customize(filtered)
+      return if filtered.empty?
+      result = ''
+      filtered.each { |key, value| result += "#{key}=#{value}&" }
+      result
+    end
   end
 end
